67ac48bbe37f3e9a7b3fa604ce28d7405b8fd44f
Seoeun Park <67998346+seony-dev@users.noreply.github.com>
2025-05-08 15:32:14 +0900

    Update sidebar-folder.js

--- assets/js/sidebar-folder.js
+++ assets/js/sidebar-folder.js
@@ -1,8 +1,20 @@
+// function spread(count)
+// {
+//     document.getElementById('folder-checkbox-' + count).checked = 
+//     !document.getElementById('folder-checkbox-' + count).checked
+//     document.getElementById('spread-icon-' + count).innerHTML = 
+//     document.getElementById('spread-icon-' + count).innerHTML == 'arrow_right' ?
+//     'arrow_drop_down' : 'arrow_right'
+// }
+
+// 25.05.08 - Sub Menu 하위 추가로 인한 수정.
 function spread(count)
 {
-    document.getElementById('folder-checkbox-' + count).checked = 
-    !document.getElementById('folder-checkbox-' + count).checked
-    document.getElementById('spread-icon-' + count).innerHTML = 
-    document.getElementById('spread-icon-' + count).innerHTML == 'arrow_right' ?
-    'arrow_drop_down' : 'arrow_right'
+    const checkbox = document.getElementById('folder-checkbox-' + count);
+    const icon = document.getElementById('spread-icon-' + count);
+
+    if(!checkbox || !icon) return;
+
+    checkbox.checked = !checkbox.checked;
+    icon.innerHTML = document.checked ? 'arrow_drop_down' : 'arrow_right';
 }
